2933b458c5d4805ad713700b85b84bfacff0509b
Francisco Costa <francisco100eg@gmail.com>
2022-06-29 15:21:07 -0300

    Aula2

---
+++ 1-Fundamentos do Desenvolvimento Web/Bloco05-JavaScript/aula2/script.js
@@ -0,0 +1,8 @@
+const a = document.getElementById("elementoOndeVoceEsta");
+a.parentNode.style.color = "red";
+
+const b =document.getElementById("primeiroFilhoDoFilho");
+b.innerHTML = "Você se lembra dos vídeos da aula anterior, como fazer isso?"
+
+const c = document.getElementById("pai");
+c.firstChild.style.color = "blue";
